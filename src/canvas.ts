5d55e9a2650d6d161ce41654ea72a247eb4b0f65
DRWR <douwe@oscom.nl>
2023-02-06 17:16:50 +0100

    fetchAnnouncements basics implemented

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,6 +1,19 @@
 import { Client } from "discordx";
-import { courseConfig } from "./config.js";iimport fetch from 'node-fetch'; //requests explained in https://stackoverflow.com/questions/45748476/http-request-in-typescript
+import { courseConfig } from "./config.js";
+import fetch from 'node-fetch';
 
+type Announcement = {
+  id: number,
+  title: string,
+  message: string,
+  url: string,
+  author: {
+    displayName: string,
+    avatarImageUrl: string,
+  },
+  postedAt: Date,
+  contextCode: string,
+};
 
 export class Canvas {
   private interval: NodeJS.Timer | null = null;
@@ -10,10 +23,10 @@ export class Canvas {
     this.interval = setInterval(this.onInterval.bind(this), 60 * 1000);
   }
 
-  onInterval() {
+  async onInterval() {
     for (const [courseId, { guilds }] of Object.entries(courseConfig)) {
       // TODO: Fetch announcements
-      const announcements: any[] = [];
+      const announcements: Announcement[] = [];
       // TODO: Filter out old announcements
       for (const announcement of announcements) {
         // TODO: Post announcement in the configured guilds
@@ -21,17 +34,36 @@ export class Canvas {
     }
   }
 
-  async fetchAnnouncements(course_codes : string[], start_date : Date) {
+  async fetchAnnouncements(courseCodes : string[], startDate : Date | null = null): Promise<Announcement[]> {
     // (see https://canvas.instructure.com/doc/api/announcements.html for more details)
+    const url = new URL("https://canvas.utwente.nl/api/v1/announcements"); 
+    const params = url.searchParams; 
+    for (const courseCode of courseCodes) params.append("context_codes[]", courseCode);    
+    params.append("active_only", "true"); // Only include published announcements, just in case the token has those permissions
+    if(startDate) params.append("start_date", startDate.toISOString());
+    params.append("access_token", this.token); 
 
-    const params = new URLSearchParams(); 
-    for (const course_code of course_codes) {
-        params.append("context_codes[]", course_code); 
-    } 
-    params.append("active_only", "true"); //only include active 
-    params.append("start_date", start_date.toISOString()); 
+    const response = await fetch(url.toString(), { method:"GET", body:null });
 
-    const response = await fetch("https://canvas.utwente.nl/api/v1/announcements" , { method:"GET", body: params });
-    const data = await response.json();
+    // TODO: Better validate shape of announcements 
+    const json = (await response.json()) as any;
+
+    if (json.message) {
+      console.error(`INVALID RESPONSE FROM API. Request: ${url.toString()}, response: `, response);
+      return []; //TODO: Make errors more explicit.  
+    }
+
+    return json.map((raw: any) => ({
+      id: raw.id,
+      title: raw.title,
+      message: raw.message,
+      url: raw.url,
+      author: {
+        displayName: raw.author.display_name,
+        avatarImageUrl: raw.author.avatar_image_url,
+      },
+      postedAt: new Date(Date.parse(raw.posted_at)), 
+      contextCode: raw.context_code,
+    }) as Announcement);
   }
 }
