80d856c007a237de7113978a05fdbc64adb8c119
Jetse Verschuren <j.verschuren@student.utwente.nl>
2023-02-06 17:34:10 +0100

    Autoformatted Douwe's stuff

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,18 +1,18 @@
 import { Client } from "discordx";
 import { courseConfig } from "./config.js";
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 type Announcement = {
-  id: number,
-  title: string,
-  message: string,
-  url: string,
+  id: number;
+  title: string;
+  message: string;
+  url: string;
   author: {
-    displayName: string,
-    avatarImageUrl: string,
-  },
-  postedAt: Date,
-  contextCode: string,
+    displayName: string;
+    avatarImageUrl: string;
+  };
+  postedAt: Date;
+  contextCode: string;
 };
 
 export class Canvas {
@@ -34,36 +34,48 @@ export class Canvas {
     }
   }
 
-  async fetchAnnouncements(courseCodes : string[], startDate : Date | null = null): Promise<Announcement[]> {
+  async fetchAnnouncements(
+    courseCodes: string[],
+    startDate: Date | null = null
+  ): Promise<Announcement[]> {
     // (see https://canvas.instructure.com/doc/api/announcements.html for more details)
-    const url = new URL("https://canvas.utwente.nl/api/v1/announcements"); 
-    const params = url.searchParams; 
-    for (const courseCode of courseCodes) params.append("context_codes[]", courseCode);    
-    params.append("active_only", "true"); // Only include published announcements, just in case the token has those permissions
-    if(startDate) params.append("start_date", startDate.toISOString());
-    params.append("access_token", this.token); 
+    const url = new URL("https://canvas.utwente.nl/api/v1/announcements");
+    const params = url.searchParams;
+    for (const courseCode of courseCodes) {
+      params.append("context_codes[]", courseCode);
+    }
+    // Only include published announcements, just in case the token has those permissions
+    params.append("active_only", "true");
+    if (startDate) params.append("start_date", startDate.toISOString());
+    params.append("access_token", this.token);
 
-    const response = await fetch(url.toString(), { method:"GET", body:null });
+    const response = await fetch(url.toString(), { method: "GET", body: null });
 
-    // TODO: Better validate shape of announcements 
+    // TODO: Better validate shape of announcements
     const json = (await response.json()) as any;
 
     if (json.message) {
-      console.error(`INVALID RESPONSE FROM API. Request: ${url.toString()}, response: `, response);
-      return []; //TODO: Make errors more explicit.  
+      console.error(
+        `INVALID RESPONSE FROM API. Request: ${url.toString()}, response: `,
+        response
+      );
+      return []; //TODO: Make errors more explicit.
     }
 
-    return json.map((raw: any) => ({
-      id: raw.id,
-      title: raw.title,
-      message: raw.message,
-      url: raw.url,
-      author: {
-        displayName: raw.author.display_name,
-        avatarImageUrl: raw.author.avatar_image_url,
-      },
-      postedAt: new Date(Date.parse(raw.posted_at)), 
-      contextCode: raw.context_code,
-    }) as Announcement);
+    return json.map(
+      (raw: any) =>
+        ({
+          id: raw.id,
+          title: raw.title,
+          message: raw.message,
+          url: raw.url,
+          author: {
+            displayName: raw.author.display_name,
+            avatarImageUrl: raw.author.avatar_image_url,
+          },
+          postedAt: new Date(Date.parse(raw.posted_at)),
+          contextCode: raw.context_code,
+        } as Announcement)
+    );
   }
 }
