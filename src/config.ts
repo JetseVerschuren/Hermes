4ae8c463a372b8267a45bcd01ecc458f24e6b3c0
Jetse Verschuren <j.verschuren@student.utwente.nl>
2024-02-07 11:52:18 +0100

    Added new course code to config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,3 @@
-import dotenv from "dotenv";
-
 type GuildConfig = {
   [guildId: string]: {
     announcementChannel: string;
@@ -8,9 +6,15 @@ type GuildConfig = {
 };
 
 export const guildConfig: GuildConfig = {
+  // Prod
   "1072100983989076098": {
     announcementChannel: "1072108873818832939",
     logChannel: "1072108615672008734",
+  },
+  // Test
+  "874599601078935612": {
+    announcementChannel: "1072194536371130450",
+    logChannel: "1072317751420530740",
   },
 };
 
@@ -22,6 +26,9 @@ type CourseConfig = {
 
 export const courseConfig: CourseConfig = {
   course_12067: {
+    guilds: ["1072100983989076098"],
+  },
+  course_14337: {
     guilds: ["1072100983989076098"],
   },
 };
